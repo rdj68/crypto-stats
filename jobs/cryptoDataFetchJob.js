8bbccd05cb5e4b06ea417d35f3b6e305c55ab988
Rajeev Singh <rsinghh68@gmail.com>
2025-01-10 21:46:01 +0530

    refactor: Move fetchCryptoPrices and mapCryptoData function to utils/cryptoUtils for reusability

--- jobs/cryptoDataFetchJob.js
+++ jobs/cryptoDataFetchJob.js
@@ -1,34 +1,6 @@
-import axios from 'axios';
+'use strict'
 import CryptoData from '../models/cryptoData.js';
-
-// Fetch crypto prices from CoinGecko API
-const fetchCryptoPrices = async (coins) => {
-  const apiUrl = process.env.COIN_GECKO_API_URL || 'https://api.coingecko.com/api/v3/simple/price';
-
-  try {
-    const { data } = await axios.get(apiUrl, {
-      params: {
-        ids: coins.join(','),
-        vs_currencies: 'usd',
-        include_market_cap: true,
-        include_24hr_change: true,
-      },
-    });
-    return data;
-  } catch (error) {
-    throw new Error('Error fetching data from CoinGecko: ' + error.message);
-  }
-};
-
-// Map the fetched crypto data into a suitable format for saving
-const mapCryptoData = (coins, data) => {
-  return coins.map(coin => ({
-    coin,
-    price: data[coin]?.usd || 0,
-    marketCap: data[coin]?.usd_market_cap || 0,
-    change24h: data[coin]?.usd_24h_change || 0,
-  }));
-};
+import { fetchCryptoPrices, mapCryptoData } from '../utils/cryptoUtils.js';
 
 // Function to save the mapped crypto data to the database
 const saveCryptoData = async (cryptoData) => {
