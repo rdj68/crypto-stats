24adc9ab1e285a41a9e87fdb1de31a5bd442caa2
Rajeev Singh <rsinghh68@gmail.com>
2025-01-10 20:28:25 +0530

    feat: Add dotenv support and .env.example file for environment variable management

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import Fastify from 'fastify';
 import hello from './routes/hello.js';
 import logger from './plugin/logger.js';
+import { configDotenv } from 'dotenv';
 
 const fastify = Fastify({
   logger: true,
 });
 
+// Load environment variables
+configDotenv();
+
+// Register plugins
 fastify.register(logger)
+
+// Register routes
 fastify.register(hello)
 
 fastify.listen({ port: 3000 }, (err, address) => {
